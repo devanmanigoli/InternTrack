a589e523bbb22fa0e06619c95d6941e1b0f8509e
devanmanigoli <devanmanigoli@gmail.com>
2026-09-24 22:42:43 +0530

    Add comment to InterntrackApplicationTests

--- backend/src/test/java/com/interntrack/interntrack/InterntrackApplicationTests.java
+++ backend/src/test/java/com/interntrack/interntrack/InterntrackApplicationTests.java
@@ -2,6 +2,7 @@ package com.interntrack.interntrack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//
 
 @SpringBootTest
 class InterntrackApplicationTests {
